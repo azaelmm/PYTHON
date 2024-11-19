b47cbbb98770f6a4d71eac89391d90a03cdeb0e0
Azael Morell <azaelmorell15@gmail.com>
2024-11-19 13:32:47 +0100

    Commit_Classes

--- classes.py
+++ classes.py
@@ -5,14 +5,25 @@ class MyEmptyPerson:
 
 
 class Person:
-    def __init__(self, name, surname):
-        self.name = name
+    def __init__(self, name, surname, alias = "Sin alias"):
+        self.__name = name
         self.surname = surname    
-        self.full_name = f"{name} {surname}"
+        self.full_name = f"{name} {surname} ({alias})"
+
+    def get_name (self):
+        return self.__name
 
     def walk(self):
-        print(f"Esta caminando {self.name}...")
+        print(f"Esta caminando {self.__name}...")
 
 my_person = Person("Azael", "Morell")
-print(f"{my_person.name} {my_person.surname}")
+print(f"{my_person.get_name} {my_person.surname}")
 print(my_person.full_name)
+my_person.walk()
+
+my_other_person = Person("Azael", "morell", "AzaelDev")
+print(my_other_person.full_name)
+my_other_person.walk()
+
+my_other_person.full_name = "Oscar Garcia (El ciclista)"
+print(my_other_person.full_name) 
